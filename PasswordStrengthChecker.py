9aad633c4ea8935bee49295547ed7eae89941601
Rohit Pravin Shinde <rohitshinde7922@gmail.com>
2026-01-13 23:57:30 +0530

    Increase password length requirement and scoring

--- PasswordStrengthChecker.py
+++ PasswordStrengthChecker.py
@@ -2,7 +2,7 @@ import re
 
 password = input("Enter password: ")
 
-length = len(password) >= 8
+length = len(password) >= 10
 upper = bool(re.search(r"[A-Z]", password))
 lower = bool(re.search(r"[a-z]", password))
 digit = bool(re.search(r"\d", password))
@@ -10,9 +10,10 @@ symbol = bool(re.search(r"[@$!%*?&#]", password))
 
 score = sum([length, upper, lower, digit, symbol])
 
-if score == 5:
+if score == 7:
     print("Strong Password 💪")
-elif score >= 3:
+elif score >= 5:
     print("Medium Password 😐")
 else:
     print("Weak Password ❌")
+
