4e55a841d29ecac80ee3a88f8cb2a8b49dabac33
Rohit Pravin Shinde <rohitshinde7922@gmail.com>
2026-01-24 22:59:59 +0530

    Add BankAccount class with basic operations
    Implement BankAccount class with deposit and withdraw methods, and handle insufficient funds.

---
+++ bank.py
@@ -0,0 +1,24 @@
+class InsufficientFunds(Exception):
+    pass
+
+class BankAccount:
+    def __init__(self, owner, balance=0):
+        self.owner = owner
+        self.balance = balance
+
+    def deposit(self, amount):
+        self.balance += amount
+        print("💰 Deposited:", amount)
+
+    def withdraw(self, amount):
+        if amount > self.balance:
+            raise InsufficientFunds("❌ Not enough balance")
+        self.balance -= amount
+        print("💸 Withdrawn:", amount)
+
+acc = BankAccount("Rohit", 5000)
+
+try:
+    acc.withdraw(6000)
+except InsufficientFunds as e:
+    print(e)
