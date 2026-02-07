f7df7c0d2b13175edfba850304f03f8dedaf6660
Rohit Pravin Shinde <rohitshinde7922@gmail.com>
2026-02-07 22:29:02 +0530

    Implement contact book functionality with JSON storage

---
+++ ContactBook.py
@@ -0,0 +1,23 @@
+import json
+
+FILE = "contacts.json"
+
+def load():
+    try:
+        with open(FILE) as f:
+            return json.load(f)
+    except:
+        return {}
+
+def save(data):
+    with open(FILE, "w") as f:
+        json.dump(data, f)
+
+contacts = load()
+
+name = input("Enter name: ")
+phone = input("Enter phone: ")
+contacts[name] = phone
+save(contacts)
+
+print("Saved successfully!")
