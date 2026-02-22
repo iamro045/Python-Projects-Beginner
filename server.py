7e8f16acb006d269477412e6480eda5f154ee0a4
Rohit Pravin Shinde <rohitshinde7922@gmail.com>
2026-02-22 19:37:46 +0530

    Update server.py

--- server.py
+++ server.py
@@ -5,8 +5,8 @@ import socket
 # 2. Right Click -> Properties -> Advanced -> Address
 # 3. Turn on Bluetooth on both devices and make server device visible
 
-server = socket.socket(socket.AF_BLUETOOTH, socket.SOCK_STREAM, socket.BTPROTO_RFCOMM)  # RFCOMM specific protocol
-server.bind(("<your BT adapter MAC address>", 4))  # MAC Address and Channel 4
+server = socket.socket(socket.AF_BLUETOOTH, socket.SOCK_STREAM, socket.BTPROTO_RFCOMM)
+server.bind(("<your BT adapter MAC address>", 4))
 server.listen(1)
 
 print("Waiting for connection...")
@@ -22,6 +22,7 @@ try:
         print(f"Received: {data.decode('utf-8')}")
         message = input("Enter message: ")
         client.send(message.encode('utf-8'))
+        
 except OSError:
     pass
 
